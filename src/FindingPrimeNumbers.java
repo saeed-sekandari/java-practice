5267f8b212b8bc9a0dc814e39bf422590b826a42
Saeed Sekandari <sekandari2003@gmail.com>
2026-06-11 14:21:17 -0700

    Add prime number generator

---
+++ src/FindingPrimeNumbers.java
@@ -0,0 +1,31 @@
+public class FindingPrimeNumbers{
+    //find the first 50 prime numbers
+    public static void main(String[] args) {
+        System.out.println("The first 50 prime numbers are ");
+        printFirstNPrimes(50);
+
+    }
+    public static boolean isPrime(int n){
+
+        if(n <= 1){
+            return false;
+        } for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void printFirstNPrimes(int n){
+        int count = 0;
+        int number = 2;
+
+        while (count < n){
+            if(isPrime(number)){
+                System.out.println(number);
+                count++;
+            }
+            number++;
+        }
+    }
+}
